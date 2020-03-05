7073f828499a5aaa34f3b878c4ab080d54ac2ad5
Matheus Costa Silva <matheus.ttu7@gmail.com>
2020-03-05 20:34:52 -0300

    Codigo completo

---
+++ TresFrasesAoContrario.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+public class TresFrasesAoContrario {
+   public static void main (String [] args) {
+      //Declaração de variáveis
+      String palavra1;
+      String palavra2;
+      String palavra3;
+      //Entrada de dados
+      palavra1 = JOptionPane.showInputDialog("Digite a primeira palavra");
+      palavra2 = JOptionPane.showInputDialog("Digite a segunda palavra");
+      palavra3 = JOptionPane.showInputDialog("Digite a terceira palavra");
+      //Processamento
+      String resultado = palavra3 + " " + palavra2 + " " + palavra1;
+      //Saída
+      JOptionPane.showMessageDialog(null, resultado);
+   }
+}
