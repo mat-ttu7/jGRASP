58fbe546803a7d508caf35ff41936c221e639a80
Matheus Costa Silva <matheus.ttu7@gmail.com>
2020-03-10 14:26:39 -0300

    Codigo completo

---
+++ ExibeData.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+public class ExibeData {
+   public static void main (String [] args) {
+      String dia = JOptionPane.showInputDialog("Digite um dia");
+      String mes = JOptionPane.showInputDialog("Digite um mes");
+      String ano = JOptionPane.showInputDialog("Digite um ano");
+      
+      String data = dia + "/" + mes + "/" + ano;
+      JOptionPane.showMessageDialog(null, data);
+   }
+}
